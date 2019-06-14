5355b258246f440adc4b7b36a449da27409e839e
pferrervich <pferrervich@gmail.com>
2019-06-14 08:37:54 +0200

    docker config & fixed fib typo

--- worker/index.js
+++ worker/index.js
@@ -10,7 +10,7 @@ const redisClient = redis.createClient({
 const sub = redisClient.duplicate();
 
 function fib(index) {
-    if (indez < 2 ) return 1;
+    if (index < 2 ) return 1;
     return fib(index - 1) + fib(index - 2);
 }
 
